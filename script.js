cb8c6120a9f4cfbd85c777a8f83e0ae2fdb64177
Saniya Shaikh <51195476+sanu29@users.noreply.github.com>
2022-08-09 12:23:02 +0530

    spriteAnimation
    not all of the code came from the course
    I have done some modifications
    dont know it will work on later on as we merge the different projects
    but hell with that as my motive is to practice:)

--- script.js
+++ script.js
@@ -1,27 +1,76 @@
+playerState = "idle"
 const canvas = document.getElementById('canvas1');
+const dropdown = document.getElementById("animations")
+dropdown.addEventListener("change", (e) => {
+    playerState = e.target.value
+}
+)
 
 const ctx = canvas.getContext("2d");
 console.log(canvas)
-const CANVAS_WIDTH = canvas.width = 300
-const CANVAS_HEIGHT = canvas.height = 300
+const CANVAS_WIDTH = canvas.width = 600
+const CANVAS_HEIGHT = canvas.height = 600
 
 const playerImage = new Image()
 playerImage.src = "shadow_dog.png"
 const spriteWidth = 575
 const spriteHeight = 523
-let frameX = 0
-let frameY = 0
-let gameFrame = 0
+var frameX
+var frameY
+let gameFrame = 0    // change this value
 const staggerFrames = 5
+playerState = "idle"
+
+spriteAnimation = {
+    idle : {
+        positionY: 0,
+        frames: 7
+    },
+    jump :{
+        positionY: 1,
+        frames: 7
+    },
+    fall :{
+        positionY: 2,
+        frames: 7
+    },
+    run : {
+        positionY: 3,
+        frames: 9
+    },
+    dizzy : {
+        positionY: 4,
+        frames: 11
+    },
+    sit : {
+        positionY: 5,
+        frames: 5
+    },
+    roll : {
+        positionY: 6,
+        frames: 7
+    },
+    bite : {
+        positionY: 7,
+        frames: 7
+    },
+    ko : {
+        positionY: 8,
+        frames: 12
+    },
+    getHit : {
+        positionY: 9,
+        frames: 4
+    }
+};
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
-    //ctx.drawImage(image, sx,sy,sw,sh, dx,dy,dw,dh
-    ctx.drawImage(playerImage, frameX*spriteWidth, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH-(25/2),CANVAS_HEIGHT-(77/2))
-    if (gameFrame % staggerFrames == 0){
-        if (frameX < 6) frameX++
-        else frameX = 0
-    }
+    position = Math.floor(gameFrame/staggerFrames) % spriteAnimation[playerState].frames
+    frameX = position * spriteWidth
+    frameY = spriteAnimation[playerState].positionY
+    ctx.drawImage(playerImage, frameX, frameY*spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH,CANVAS_HEIGHT)
+
 
     gameFrame++
     requestAnimationFrame(animate)
